32088531a42d920f0ccca62952b47d13c55dff95
Aditya Garg <adityagarg733@gmail.com>
2025-10-05 10:16:54 +0530

    setting up update and delete controller

--- backend/src/controllers/shipmentController.js
+++ backend/src/controllers/shipmentController.js
@@ -49,3 +49,52 @@ export const getShipments = async (req, res) => {
   }
 };
 
+export const updateShipment = async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log('hello1');
+    if (!id) return res.status(400).json({ error: "Shipment ID is required" });
+
+    const { newdescription, newstatus } = req.body;
+    console.log('hello2');
+    if (!newdescription || !newstatus) 
+      return res.status(400).json({ error: "New description and status are required" });
+
+    const owner = await Shipment.findById(id).select("owner");
+    console.log('hello3');
+    if (!owner) return res.status(404).json({ error: "Shipment not found" });
+
+    // Correct comparison
+    console.log(owner.owner.toString(), req.user);
+    if (owner.owner.toString() !== req.user)
+      return res.status(401).json({ error: "Unauthorized" });
+
+    const shipment = await Shipment.findByIdAndUpdate(
+      id,
+      { description: newdescription, status: newstatus },
+      { new: true }
+    );
+    console.log('hello4');
+
+    res.json(shipment);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ error: "Failed to update shipment" });
+  }
+}; 
+
+export const deleteShipment = async (req, res) => {
+  try {
+    console.log('hello') ;
+    const { id } = req.params;
+    console.log('hello2') ;
+    if(!id) return res.status(400).json({ error: "Shipment ID is required" });
+    const shipment = await Shipment.findByIdAndDelete(id);
+    if (!shipment) {
+      return res.status(404).json({ error: "Shipment not found" });
+    }
+    res.json({ message: "Shipment deleted successfully" });
+  } catch (err) {
+    res.status(400).json({ error: "Failed to delete shipment" });
+  }
+};
